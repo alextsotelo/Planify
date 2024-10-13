2791c06058f1e236e2bd53136c5a9b80d327eb75
alextsotelo <alextsotelo@gmail.com>
2024-10-13 18:40:46 -0500

    first commit

---
+++ app/src/main/java/com/example/calendarapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.calendarapp;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            // Comment out the next line to disable the Toast
+            // String date = dayOfMonth + "/" + (month + 1) + "/" + year; // Month is 0-indexed
+            // Toast.makeText(MainActivity.this, "Selected date: " + date, Toast.LENGTH_SHORT).show();
+        });
+    }
+}
